a21784c9491136cd63e4f97006b8151a0fc0fdbf
Duncan <99960161+DuncanGuijo@users.noreply.github.com>
2023-01-31 15:11:04 +0100

    Update boot.py

--- boot.py
+++ boot.py
@@ -12,31 +12,28 @@ web = webdriver.Chrome() #añadimos a la variable web el explorador Chrome
 web.get('http://instagram.com') #abrimos instagram
 time.sleep(5) #esperamos 
 
-#función de inicio de sesios
+#función de inicio de sesión
   
 def login():
     user = "" #tu usuario
     paswd = "" #tu password
-    aceptarCookies = web.find_element(By.XPATH, "//button[contains(text(),'Permitir cookies necesarias y opcionales')]") #buscamos boton de coockies
-    aceptarCookies.click() #aceptamos las cockies
+    aceptarCookies = web.find_element(By.XPATH, "//button[contains(text(),'Permitir cookies necesarias y opcionales')]") #buscamos botón de coockies
+    aceptarCookies.click() #aceptamos las cookies
     time.sleep(5)
     inputUser = web.find_element("name", "username") #mediante el nombre seleccionamos el campo del usuario
     inputUser.send_keys(user) #lo introducimos
-    inputPass = web.find_element("name", "password") #mediante el nombre seleccionamos el campo de la pass
+    inputPass = web.find_element("name", "password") #mediante el nombre seleccionamos el campo de la conntraseña
     inputPass.send_keys(paswd) #la introducimos
     time.sleep(5)
-    loginButton = web.find_element(By.XPATH, "//button[@type='submit']").click() #buscamos el primer boton del tipo submit y lo clicamos
+    loginButton = web.find_element(By.XPATH, "//button[@type='submit']").click() #buscamos el primer botón de tipo submit y lo clicamos
     time.sleep(5)
     print("Salgo del login")
  
 #función abrir folowers
  
-def open_followers(account_instagram): #account instagram = al campo que le pasamos en la linea 79
-    print("Entro a open_followers")
-    web.get("https://www.instagram.com/" + account_instagram + "/followers/") #abrimos en insta la pagina de los followers que ya sale el pop_up (ventana emergente)
-    time.sleep(5)
-    print("Salgo de open_followers")
- 
+def open_followers(account_instagram): 
+    web.get("https://www.instagram.com/" + account_instagram + "/followers/") #abrimos en instagram los seguidores de la página que hayamos indicado
+    time.sleep(5) 
  
 #función seguir cuentas
 
@@ -44,9 +41,9 @@ def follow_followers(seguidores):
     time.sleep(5)
     i = 1 
     j = 5
-    #localizamos el primet boton "Seguir" mediante la variable i iteraremos sobre esta ruta
+    #localizamos el primet boton "Seguir" mediante la variable "i" e iteraremos sobre esta ruta
     button = web.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[" + str(i) + "]/div[3]/button")
-    while(button and i < seguidores):
+    while(button and i < seguidores): #mientras i sea menor a la cantidad de seguidores que queremos entramos en el bucle
         print("vuelta: " + str(i))
         if(i > j):#en caso de que j sea mayor que i haremos scroll en el pop up e incrementaremos la variable j
             print("5 segundos")
@@ -67,9 +64,9 @@ def follow_followers(seguidores):
 
  
 sources = [""] #Array donde almacenamos las paginas a las que queremos ir
-login() #invocamos a la funcion login
+login() #invocamos la función login
 time.sleep(5) #esperamos 5 segundos
 for account in sources: # dentro del array sources buscamos cada parametro y lo asociamos a la variable account
-    open_followers(account) #pasamos la variable account a la funcion followers
+    open_followers(account) #pasamos la variable account a la función followers
     time.sleep(5) #esperamos
-    follow_followers(30) #invocamos a la funcion follow y le pasamos la cantidad de seguidores que queremos
+    follow_followers(30) #invocamos a la función follow y le pasamos la cantidad de seguidores que queremos
